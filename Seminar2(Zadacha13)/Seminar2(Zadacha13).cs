54abeb0f6290a9435ab68d49d4b6b66fedeb05ba
fale19941 <valera.sant@mail.ru>
2023-06-16 20:32:23 +0300

    Задачи 2 семинара выполнены успешно

---
+++ Seminar2(Zadacha13)/Seminar2(Zadacha13).cs
@@ -0,0 +1,26 @@
+int number = ReadInt("Введите число");
+int count = number.ToString().Length;
+Console.Write(MakeArray(number, count));
+int ReadInt(string message)
+{
+    Console.Write(message);
+return Convert.ToInt32(Console.ReadLine());
+}
+int MakeArray(int a, int b)
+{
+    int result = 0;
+    if (b< 3)
+{
+    Console.Write("Третьей цифры нет, держи");
+}
+else
+{
+    int c = 1;
+    for (int i = b; i > 3; i--);
+{
+
+}
+result =(a / c) % 10;
+}
+return result;
+}
